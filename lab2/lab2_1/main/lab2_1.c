358ea9bf54854b92e08ebe53a46a3b8a2dd5128f
Ryan King <gamingskills2002@gmail.com>
2024-05-25 17:06:39 -0700

    updates

--- lab2/lab2_1/main/lab2_1.c
+++ lab2/lab2_1/main/lab2_1.c
@@ -2,5 +2,8 @@
 
 void app_main(void)
 {
-
+    printf("Hello world!\n");
+    printf("This is my first ESP32 program.\n");
+    printf("I am going to use ESP32 to do some interesting projects.\n");
+    printf("I am looking forward to it.\n");
 }
